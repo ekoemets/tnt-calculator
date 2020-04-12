ff50f3556cb2f6c01638bc06a97f0dc645ba704b
emilkoemets <emilkoemets@gmail.com>
2020-04-12 11:05:42 +0300

    Updates

---
+++ precache-manifest.c60da6be8f21cd4e3dbe060d60c2b1fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3491279ebc8f00c2aaba68d770f0bd8b",
+    "url": "/tnt-calculator/index.html"
+  },
+  {
+    "revision": "7b89e473b211fcca143a",
+    "url": "/tnt-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "315cc608956a3e2d48f0",
+    "url": "/tnt-calculator/static/js/2.5f012942.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tnt-calculator/static/js/2.5f012942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b89e473b211fcca143a",
+    "url": "/tnt-calculator/static/js/main.f276324b.chunk.js"
+  },
+  {
+    "revision": "ed144c3dc197e6d08b2c",
+    "url": "/tnt-calculator/static/js/runtime-main.09d09539.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tnt-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
