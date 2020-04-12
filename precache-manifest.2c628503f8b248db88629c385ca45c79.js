c4583bd1f9c871ba14ece774daf703603deaf86e
emilkoemets <emilkoemets@gmail.com>
2020-04-12 16:09:17 +0300

    Updates

---
+++ precache-manifest.2c628503f8b248db88629c385ca45c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ba1d6c418c862a54e4ed404d9287be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07cbaa21b6f6c63682ff",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c70f972e164c3beb59fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07cbaa21b6f6c63682ff",
+    "url": "/static/js/2.dac5587b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dac5587b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70f972e164c3beb59fc",
+    "url": "/static/js/main.0e818532.chunk.js"
+  },
+  {
+    "revision": "52ccc24f0dbf368eef13",
+    "url": "/static/js/runtime-main.04ef483f.js"
+  }
+]);
